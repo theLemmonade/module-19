5dbc755d9661d5f314e925ce901a3dfbbc62c082
theLemmonade <lemmonmail@icloud.com>
2023-03-08 16:20:28 -0500

    configured wepback, node install

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -5,20 +5,64 @@ const { InjectManifest } = require("workbox-webpack-plugin");
 const { type } = require("os");
 
 module.exports = () => {
-	return {
-		mode: "development",
-		entry: {
-			main: "./src/js/index.js",
-			install: "./src/js/install.js",
-		},
-		output: {
-			filename: "[name].bundle.js",
-			path: path.resolve(__dirname, "dist"),
-		},
-		plugins: [],
+  return {
+    mode: 'development',
+    entry: {
+      main: './src/js/index.js',
+      install: './src/js/install.js'
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Jate',
+      }),
+      new InjectManifest ({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Jate',
+        short_name: 'Jate',
+        description: 'A text editor for your browser',
+        background_color: '#01579b',
+        theme_color: '#01579b',
+        start_url: './',
+        display: 'standalone',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+    ]
+      })
+    ],
 
-		module: {
-			rules: [],
-		},
-	};
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
+ ],
+    },
+  };
 };

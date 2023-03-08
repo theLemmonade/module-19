9135eed6dea387c0d1f9f2466496017a92ac1390
theLemmonade <lemmonmail@icloud.com>
2023-03-08 16:17:32 -0500

    init commit

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,30 +1,24 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
-
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+const { InjectManifest } = require("workbox-webpack-plugin");
+const { type } = require("os");
 
 module.exports = () => {
-  return {
-    mode: 'development',
-    entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
-    },
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-      
-    ],
+	return {
+		mode: "development",
+		entry: {
+			main: "./src/js/index.js",
+			install: "./src/js/install.js",
+		},
+		output: {
+			filename: "[name].bundle.js",
+			path: path.resolve(__dirname, "dist"),
+		},
+		plugins: [],
 
-    module: {
-      rules: [
-        
-      ],
-    },
-  };
+		module: {
+			rules: [],
+		},
+	};
 };

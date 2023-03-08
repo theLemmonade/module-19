b70469e08bdc76a330b78d2987e255484d6e944e
theLemmonade <lemmonmail@icloud.com>
2023-03-08 16:09:16 -0500

    finished todo in db.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,35 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB('jate', 1, {
-    upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
-        return;
-      }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
-    },
-  });
+	openDB("jate", 1, {
+		upgrade(db) {
+			if (db.objectStoreNames.contains("jate")) {
+				console.log("jate database already exists");
+				return;
+			}
+			db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+			console.log("jate database created");
+		},
+	});
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+	const contactsDb = await initdb("jate", 1);
+	const tx = contactsDb.transaction("jate", "readwrite");
+	const store = tx.objectStore("jate");
+	const request = await store.put({ value: content });
+	const result = await request;
+	console.log(result);
+	return result;
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+	const contactsDb = await initdb("jate", 1);
+	const tx = contactsDb.transaction("jate", "readonly");
+	const store = tx.objectStore("jate");
+	const request = await store.getAll();
+	const result = await request;
+	console.log(result);
+	return result;
+};
 
 initdb();
